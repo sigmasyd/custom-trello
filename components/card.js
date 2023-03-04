89610dc1dee5678c20cbfc690d87f08a76a41a14
Carlos Barcos <fbarcos8@hotmail.com>
2023-03-04 11:18:50 -0500

    Agregando imagenes y lista de cards

---
+++ components/card.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Image from 'next/image'
+
+export default function Card({ text }) {
+  return (
+    <div className='bg-slate-100 text-slate-900 rounded-md m-2 p-3 flex flex-col gap-4'>
+      <div className='flex justify-between'>
+        <p className='font-medium'>{ text }</p>
+        <Image src='/static/icons/edit.svg' alt="" width={20} height={20} />
+      </div>
+      <div className='flex justify-between'>
+        <Image src='/static/images/avatar.png' alt="" width={20} height={20} />
+        <Image src='/static/icons/square.svg' alt="" width={20} height={20} />
+      </div>
+    </ div>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,31 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import List from "../components/list"
+import Card from "../components/card"
+
+const cardList = [
+  {
+    text: 'Implementar inicio de sesión'
+  },
+  {
+    text: 'Guardar datos en base de datos'
+  },
+  {
+    text: 'Implementar inicio de sesión'
+  },
+]
 
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline text-blue-700">
-      Hello world!
-    </h1>
+    <>
+      <h1 className="text-3xl font-bold underline text-ellipsis">
+        Hello world!
+      </h1>
+      <List>
+        {
+          cardList.map( (item, index) => (
+            <Card {...item} key={index}/>
+          ))
+        }
+      </List>
+    </>
   )
 }
